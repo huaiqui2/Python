e9f09284ed6e9fa9352247276b55871d6101c88b
huaiqui2 <91351793+huaiqui2@users.noreply.github.com>
2021-10-01 21:13:30 -0300

    Add files via upload

---
+++ Contadores.py
@@ -0,0 +1,21 @@
+varNumero1 = 0
+varIncrementador = 1
+
+varNumero1 = varNumero1 + varIncrementador
+
+print(varNumero1)
+print("---------------------------------------------------------")
+
+varRango1 = 500
+varRango2 = 0
+
+for varCont in range(varRango1,varRango2):
+    print("la secuencia es:"+str(varCont))
+
+
+print("-------------------------------------------------------")
+
+varArreglo = [a,e,i,o,u]
+
+for varCont in varArreglo:
+    print("las vocales son:"+str(varCont))
